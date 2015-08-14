b10fc333fc281855373a52f06e0c9959f578aef1
Randolph Kahle <randy@gcphq.roitraining.com>
2015-08-14 02:46:57 +0000

    Added a connection to project_id as an environment variable

--- build.sh
+++ build.sh
@@ -6,9 +6,13 @@
 #          using the guestbook Docker container
 #
 
+PROJECT_ID=$(curl -s 'http://metadata/computeMetadata/v1/project/project-id' -H 'Metadata-Flavor: Google')
+
 gcloud compute images delete guestbook-image
 
 # Issue meta requests to fill in missing information in the compute-image-build.json file
 
 # Run Packer to build the image
-sudo /opt/packer/packer build compute-image-build.json
+sudo /opt/packer/packer build \
+  -var 'project_id=${PROJECT_ID}'
+  compute-image-build.json
